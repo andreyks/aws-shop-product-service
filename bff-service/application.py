0ad69706199ed99e113e8d11078f0b4d8d30f23a
Andrii Kostromin <andrii_kostromin@epam.com>
2025-04-24 00:28:24 +0300

    Task 10: BFF: debug python app

--- bff-service/application.py
+++ bff-service/application.py
@@ -1,18 +1,23 @@
 from flask import Flask, request, Response
 import requests
+import logging
+import json
 
 application = Flask(__name__)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 INTERNAL_API_URL = "https://cj1fjt38ve.execute-api.eu-north-1.amazonaws.com/development"
 
 @application.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'])
 @application.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'])
 def proxy(path):
-    print(f"Received request for path: {path}");
-    print(f"Request method: {request.method}");
-    print(f"Request headers: {request.headers}")
-    print(f"Request cookies: {request.cookies}")
-    print(f"Request data len: {len(request.get_data())}")
+    logger.info(f"## Received request for path: {path}")
+    logger.info(f"## Request method: {request.method}")
+    logger.info(f"## Request headers: {request.headers}")
+    logger.info(f"## Request cookies: {request.cookies}")
+    logger.info(f"## Request data len: {len(request.get_data())}")
 
     # Construct the URL for the internal API
     url = f"{INTERNAL_API_URL}/{path}"
